05d4f58535c417a7816d9e31233553eb513ea2c4
manziro785 <manziro785@gmail.com>
2024-09-16 10:23:39 +0600

    add pict

--- src/pages/noPage/noPage.jsx
+++ src/pages/noPage/noPage.jsx
@@ -4,6 +4,8 @@ import Header2 from '../../components/header2/header2'
 import Footer from '../../components/footer/footer'
 import img11 from '../../assets/tablepage/Frame 38.svg'
 import img12 from '../../assets/tablepage/Frame 39 (1).svg'
+import img13 from '../../assets/tablepage/Frame 40 (2).svg'
+
 import { NavLink } from 'react-router-dom'
 
 
@@ -15,6 +17,12 @@ export default function NoPage() {
 <div className="wrapper_time">
   <div className="img">
     <img src={img11} alt="" />
+  </div>
+  <div className="img">
+    <img src={img12} alt="" />
+  </div>
+  <div className="img">
+    <img src={img13} alt="" />
   </div>
   <div className="btns_result">
     <NavLink to = '/table'>
